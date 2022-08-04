57b05be82604b8b36f1b9e323ffa53933bccbfec
Tan Luong <tandluong@yahoo.com>
2022-08-03 20:20:50 -0600

    Completed Exercise2_11

--- gettingback/Chapter2/Exercise2_11.java
+++ gettingback/Chapter2/Exercise2_11.java
@@ -1,17 +1,35 @@
 import java.util.*;
-
 public class Exercise2_11 {
-    double population = 312032486;
-    double oneYear = 365.0;
-    double oneDay = 24.0;
-    double oneHour = 3600.0;
-    double secondsInOneYear = oneHour * oneDay * oneYear;
-    double birthPerSecond = 7.0;
-    double deathtPerSecond = 13.0;
-    double immigrantPerSecond = 45.0;
-    double numberOfBirthPerYear = secondsInOneYear / birthPerSecond;
-    double numberOfDeathPerYear = secondsInOneYear / deathtPerSecond;
-    double numberOfImmigrantPerYear = secondsInOneYear / immigrantPerSecond;
+    public static void main(String[] args) {
+        // create Scanner Object
+        Scanner input = new Scanner(System.in);
+        // prompt user for input
+        System.out.print("Enter the number of years: ");
+        int numberOfYears = input.nextInt();
 
+        // initial population
+        double population = 312032486;
+        // how many days are in a year
+        double oneYear = 365.0;
+        // how many hours are in a day
+        double oneDay = 24.0;
+        // how many seconds are in an hour
+        double oneHour = 3600.0;
+        // calculate how many seconds are in a year
+        double secondsInOneYear = oneHour * oneDay * oneYear;
+        // given rates
+        double birthPerSecond = 7.0;
+        double deathPerSecond = 13.0;
+        double immigrantPerSecond = 45.0;
+        // calculate each variable by dividing the amount of seconds in one year by each given rate
+        double numberOfBirthPerYear = secondsInOneYear / birthPerSecond;
+        double numberOfDeathPerYear = secondsInOneYear / deathPerSecond;
+        double numberOfImmigrantPerYear = secondsInOneYear / immigrantPerSecond;
 
+        // calculate the projection of the population based on how many years
+        double projectedPopulation = population + (Math.round((numberOfBirthPerYear + numberOfImmigrantPerYear - numberOfDeathPerYear) * numberOfYears));
+
+        // print result
+        System.out.println("The population in " + numberOfYears + " is " + (int)projectedPopulation);
+    }
 }
